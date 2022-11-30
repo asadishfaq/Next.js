c8141a41e523a20643b08f8720b5b84ef8958619
Asad Ishfaq <asadishfaq@aquila360.com>
2022-11-30 17:16:26 +0500

    implement delete functionality

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,8 +1,23 @@
 import { Fragment } from "react";
 import classes from "./MeetupDetail.module.css";
+import FlexView from "react-flexview";
+import axios from "axios";
+import notificationSvc from "../../services/notification-service";
+import { useRouter } from "next/router";
 
 function MeetupDetail(props) {
-  const { name, image, age, address, email } = props.meetupData;
+  const { name, id, age, address, email } = props.meetupData;
+  const router = useRouter();
+
+  const handleDeleteUser = async () => {
+    const URL = `http://localhost:5000/auth/${id}`;
+    const deleteUser = await axios.delete(URL);
+    console.log("deletedUser", deleteUser);
+    notificationSvc.success("user deleted successfully");
+    router.push("/");
+  };
+
+  const handleEditUser = () => {};
   return (
     <section className={classes.detail}>
       <img
@@ -15,6 +30,19 @@ function MeetupDetail(props) {
       <h2>{email}</h2>
       <h3>{age}</h3>
       <address>{address}</address>
+
+      <FlexView>
+        <FlexView marginTop="auto">
+          <div className={classes.actions}>
+            <button onClick={handleEditUser}>Edit</button>
+          </div>{" "}
+        </FlexView>
+        <FlexView marginLeft="auto">
+          <div className={classes.actions}>
+            <button onClick={handleDeleteUser}>Delete</button>
+          </div>
+        </FlexView>
+      </FlexView>
     </section>
   );
 }
--- pages/new-user/index.js
+++ pages/new-user/index.js
@@ -1,11 +1,16 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 import notificationSvc from "../../services/notification-service";
 import axios from "axios";
+import { useRouter } from "next/router";
+
 function NewMeetup() {
+  const router = useRouter();
+
   const onAddMeetup = async (values) => {
     const url = "http://localhost:5000/auth/signup";
     const newUser = await axios.post(url, values);
     notificationSvc.success("User Added Successfully");
+    router.push("/");
   };
   return (
     <>
